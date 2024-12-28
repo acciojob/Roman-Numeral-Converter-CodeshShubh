db1f890e647cefcad77ab995539455d053009a7b
Shubhanshu Saadhiyaan <138874847+CodeshShubh@users.noreply.github.com>
2024-12-28 17:31:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -9,12 +9,24 @@ function convertToRoman(num) {
       6:['I', 1]
     };
 
+
   //your code here
+	let romen ="";
+	for(let i=0; i<Object.keys(obj); i++){
+		const [symbol , value] = obj[i];
+            
+		while (num>=value) { 
+			romen +=symbol;
+			num -=value;
+		}
+	}
+
+	return romen;
 
 }
 // You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
 
-// console.log(convertToRoman(36));
+console.log(convertToRoman(36));
 
 
 
